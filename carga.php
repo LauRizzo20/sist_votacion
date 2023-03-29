12c423114ed59a460c373d9862a45284bb2de578
LauRizzo <laurizzo556@gmail.com>
2023-03-29 16:59:53 -0300

    fix: conexion reestructurada

--- carga.php
+++ carga.php
@@ -1,12 +1,12 @@
 <?php
-include ('connection.php');
+include ('conn.php');
 
     $ano = $_POST['ano'];
     $curso = $_POST['curso'];
     $grupo1 = $_POST['grupo1'];
     $grupo2 = $_POST['grupo2'];
 
-    if (mysqli_query($con, "INSERT INTO `tbl_curso` (`ano_curso`, `division_curso`, `grupo1_curso`, `grupo2_curso`) VALUES ('".$ano."','".$curso."','".$grupo1."','".$grupo2."')")) {
+    if (mysqli_query($conn, "INSERT INTO `tbl_curso` (`ano_curso`, `division_curso`, `grupo1_curso`, `grupo2_curso`) VALUES ('".$ano."','".$curso."','".$grupo1."','".$grupo2."')")) {
         echo "ok";
     } else {
         echo "Error";
